09b92208ce5586b80215b4a4e9b2cdd4b98b4ebd
AkashPattanaik <akashjio66666@gmail.com>
2020-10-12 20:48:44 +0530

    Create IronEye_Termux.sh

---
+++ IronEye_Termux.sh
@@ -0,0 +1,31 @@
+# /usr/root/IronEye - By AkashPattnaik
+
+#    ___                      _____
+#   |_ _| _ __   ___   _ __  | ____| _   _   ___
+#    | | | '__| / _\\ | '_ \\|  _|  | | | | / _ \\
+#    | | | |   | (_) || | | || |___ | |_| ||  __/
+#   |___||_|   \\___/ |_| |_||_____| \\__, \\___|
+#                                   |___/
+#       // Created By Akash Pattnaik \\
+#         // Telegram : @AKASH_AM1 \\
+#   // Github : github.com/BLUE-DEVIL1134 \\
+#
+# Create Date : 09/10/2020
+# Latest Update On : First Commit
+#
+# [*] The Idea -
+#         The Idea Behind This Project Is That,
+#         When I Came On Telegram, I Didn't Had A RDP or Computer
+#         And Had To Struggle For It..
+#
+#         Just Not To Happen With Any Other,
+#         This Project Was Made.
+pkg install python3-pip -y
+pkg install wget -y
+pkg install openssh openssl -y
+pkg install python -y
+pkg install -r requirements.txt
+
+# No more Requirement
+rm requirements.txt
+python -m IronEye

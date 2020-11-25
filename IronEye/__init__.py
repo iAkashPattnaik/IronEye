901ab4e93d997481d06096bd4f32456667460024
AkashPattanaik <akashjio66666@gmail.com>
2020-11-25 14:32:08 +0530

    Update __init__.py

--- IronEye/__init__.py
+++ IronEye/__init__.py
@@ -10,7 +10,7 @@
    // Github : github.com/BLUE-DEVIL1134 \\
 
 Create Date : 09/10/2020
-Latest Update On : 24/10/20
+Latest Update On : 25/11/20
 
 [*] The Idea -
         The Idea Behind This Project Is That,
@@ -19,19 +19,38 @@ Latest Update On : 24/10/20
 
         Just Not To Happen With Any Other,
         This Project Was Made.
+
+[*] Usage -
+        apt update && apt upgrade -y
+        apt install git -y
+        apt install python -y
+
+        git clone https://github.com/BLUE-DEVIL1134/IronEye.git
+        cd IronEye
+        pip install requirements.txt
+        chmod +x *
+        python -n IronEye
 """
 
-__version__ = 2.1
+__version__ = 2.7
 __email__ = "akashpattnaik.github@gmail.com"
 __author__ = "Akash Pattnaik"
 __telegram__ = "https://telegram.me/AKASH_AM1"
 __copyright__ = "(c) Akash Pattnaik 2020-2025"
 __github__ = "https://github.com/BLUE-DEVIL1134/IronEye"
-__Pypi__ = "https://pypi.org/user/AkashPattnaik/"
-__latest_update__ = "24/10/20"
+__Pypi__ = "pypi.org/profile/AkashPattnaik"
+__latest_update__ = "25/11/20"
 
-class PersonalException(ConnectionError):
+
+# Site-Packages
+from requests import get
+
+
+class PersonalException(ConnectionError, Exception):
     def __init__(self):
-        print("Seems That You Have Some Internet Problems.\n"
-              "Retrying..")
-        pass
+        try:
+            _ = get(url="https://github.com/BLUE-DEVIL1134/IronEye").text
+        except (Exception, ConnectionError):
+            print("Seems That You Have Some Internet Problems.\n"
+                  "Retrying..")
+    pass

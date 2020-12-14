5f9d4dadd7d26729b2f476295dcd19d87024d91b
AkashPattanaik <akashjio66666@gmail.com>
2020-12-14 15:20:34 +0530

    Update __init__.py

--- IronEye/__init__.py
+++ IronEye/__init__.py
@@ -10,7 +10,7 @@
    // Github : github.com/BLUE-DEVIL1134 \\
 
 Create Date : 09/10/2020
-Latest Update On : 25/11/20
+Latest Update On : 14/12/20
 
 [*] The Idea -
         The Idea Behind This Project Is That,
@@ -32,25 +32,11 @@ Latest Update On : 25/11/20
         python -n IronEye
 """
 
-__version__ = 2.7
+__version__ = 3.0
 __email__ = "akashpattnaik.github@gmail.com"
 __author__ = "Akash Pattnaik"
 __telegram__ = "https://telegram.me/AKASH_AM1"
 __copyright__ = "(c) Akash Pattnaik 2020-2025"
 __github__ = "https://github.com/BLUE-DEVIL1134/IronEye"
 __Pypi__ = "pypi.org/profile/AkashPattnaik"
-__latest_update__ = "25/11/20"
-
-
-# Site-Packages
-from requests import get
-
-
-class PersonalException(ConnectionError, Exception):
-    def __init__(self):
-        try:
-            _ = get(url="https://github.com/BLUE-DEVIL1134/IronEye").text
-        except (Exception, ConnectionError):
-            print("Seems That You Have Some Internet Problems.\n"
-                  "Retrying..")
-    pass
+__latest_update__ = "14/12/20"
